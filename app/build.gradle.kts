b1cae30a15f4a92166b523108ba1549312ab0367
M.Zareei <m.zareei@jabama.com>
2022-07-12 10:41:45 +0430

    Initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,90 @@
+import com.android.build.gradle.*
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+dependencies {
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.android.material)
+    implementation(libs.androidx.constariantlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.test.ext.junit)
+    androidTestImplementation(libs.androidx.test.espresso.core)
+}
+
+android {
+    compileSdk = 32
+    defaultConfig {
+        minSdk = 21
+        targetSdk = 32
+    }
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+fun BaseExtension.baseConfig() {
+
+    compileSdkVersion(32)
+
+    defaultConfig.apply {
+        minSdk = 21
+        targetSdk = 32
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    tasks.withType<KotlinCompile> {
+        kotlinOptions {
+            jvmTarget = "1.8"
+        }
+    }
+}
+
+fun PluginContainer.applyBaseConfig(project: Project) {
+    whenPluginAdded {
+        when (this) {
+            is AppPlugin -> {
+                project.extensions
+                    .getByType<AppExtension>()
+                    .apply {
+                        baseConfig()
+                    }
+            }
+            is LibraryPlugin -> {
+                project.extensions
+                    .getByType<LibraryExtension>()
+                    .apply {
+                        baseConfig()
+                    }
+            }
+        }
+    }
+}
+
+subprojects {
+    project.plugins.applyBaseConfig(project)
+}
