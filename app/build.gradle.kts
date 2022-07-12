e2e05cfdf4bb0a7002887c8ad3192dc66728a498
M.Zareei <m.zareei@jabama.com>
2022-07-12 11:44:43 +0430

    structure of project

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,3 @@
-import com.android.build.gradle.*
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
@@ -17,11 +14,6 @@ dependencies {
 }
 
 android {
-    compileSdk = 32
-    defaultConfig {
-        minSdk = 21
-        targetSdk = 32
-    }
     buildTypes {
         release {
             isMinifyEnabled = false
@@ -37,61 +29,5 @@ android {
     }
     kotlinOptions {
         jvmTarget = "1.8"
-    }
-}
-
-subprojects {
-    project.plugins.applyBaseConfig(project)
-}
-
-fun BaseExtension.baseConfig() {
-    compileSdkVersion(32)
-    defaultConfig.apply {
-        minSdk = 21
-        targetSdk = 32
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("consumer-rules.pro")
-    }
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-
-    tasks.withType<KotlinCompile> {
-        kotlinOptions {
-            jvmTarget = "1.8"
-        }
-    }
-
-    dependencies {
-        implementation(libs.androidx.core.ktx)
-        implementation(libs.androidx.appcompat)
-        implementation(libs.android.material)
-        implementation(libs.androidx.constariantlayout)
-        testImplementation(libs.junit)
-        androidTestImplementation(libs.androidx.test.ext.junit)
-        androidTestImplementation(libs.androidx.test.espresso.core)
-    }
-}
-
-fun PluginContainer.applyBaseConfig(project: Project) {
-    whenPluginAdded {
-        when (this) {
-            is AppPlugin -> {
-                project.extensions
-                    .getByType<AppExtension>()
-                    .apply {
-                        baseConfig()
-                    }
-            }
-            is LibraryPlugin -> {
-                project.extensions
-                    .getByType<LibraryExtension>()
-                    .apply {
-                        baseConfig()
-                    }
-            }
-        }
     }
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import com.android.build.gradle.*
+
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     id("com.android.application") version libs.versions.plugin.android.version apply false
@@ -8,5 +10,51 @@ plugins {
 tasks {
     register("clean", Delete::class) {
         delete(rootProject.buildDir)
+    }
+}
+
+subprojects {
+    project.plugins.applyBaseConfig(project)
+}
+
+fun BaseExtension.baseConfig() {
+    compileSdkVersion(32)
+    defaultConfig.apply {
+        minSdk = 21
+        targetSdk = 32
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+        kotlinOptions {
+            jvmTarget = "1.8"
+        }
+    }
+}
+
+fun PluginContainer.applyBaseConfig(project: Project) {
+    whenPluginAdded {
+        when (this) {
+            is AppPlugin -> {
+                project.extensions
+                    .getByType<AppExtension>()
+                    .apply {
+                        baseConfig()
+                    }
+            }
+            is LibraryPlugin -> {
+                project.extensions
+                    .getByType<LibraryExtension>()
+                    .apply {
+                        baseConfig()
+                    }
+            }
+        }
     }
 }
